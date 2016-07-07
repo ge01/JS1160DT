321b98d9f4576784cc4ce358186598b644031764
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-07-07 18:48:21 -0500

    The typeof Operator

---
+++ app.js
@@ -0,0 +1,10 @@
+/* JavaScript Arrays
+You can use the JavaScript typeof operator to find the type of a
+JavaScript variable::
+*/
+document.getElementById("demo").innerHTML =
+typeof "john" + "<br>" +
+typeof 3.14 + "<br>" +
+typeof false + "<br>" +
+typeof [1,2,3,4] + "<br>" +
+typeof {name:'john', age:34};
