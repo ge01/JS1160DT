e7062ef66b3eb6baaec11828e3c31d1b0763e47b
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-07-07 18:57:43 -0500

    Undefined

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* JavaScript Arrays
+/* The typeof Operator
 You can use the JavaScript typeof operator to find the type of a
 JavaScript variable::
 */
@@ -8,3 +8,11 @@ typeof 3.14 + "<br>" +
 typeof false + "<br>" +
 typeof [1,2,3,4] + "<br>" +
 typeof {name:'john', age:34};
+
+/* Undefined
+In JavaScript, a variable without a value, has the
+value undefined. The typeof is also undefined.
+*/
+var person;
+document.getElementById("undi").innerHTML =
+person + "<br>" + typeof person;
